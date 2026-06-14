91fd9b2caff253930620db3bc3fde062b365d508
CainPatel <caincollege1212@gmail.com>
2026-06-14 08:38:14 +0700

    troubleshooted bugs in the training file and added functionality to save the model weights to checkpoints

--- train.py
+++ train.py
@@ -1,8 +1,9 @@
 import torch
-import model
+from model import GPT
 from huggingface_hub import hf_hub_download
 import pandas as pd
 import torch.nn.functional as F
+import os
 
 # Tokenizer and batch architecture
 
@@ -16,10 +17,10 @@ def get_batch(data):
     ix = torch.randint(0, len(data)-block_size, (batch_size,))
 
 # creates batch_size batches to train model 
-    x = torch.stack([data[ix:ix+block_size] for i in ix])
+    x = torch.stack([data[i:i+block_size] for i in ix])
 
     # creates batch_size batches, which are shifted by 1, to test model
-    y = torch.stack([data[ix+1:ix+block_size+1] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
     return x,y
 
 # Data processing
@@ -49,13 +50,13 @@ d_model, n_heads, d_ff, n_layers, max_len = 32, 4, 128, 2, block_size
 n_epochs = 2000
 
 # creates model and optimizer object  
-gpt_model = model(vocab_size, n_heads, d_ff, n_layers, max_len)
-optimizer = torch.optim.Adam(gpt_model.parameters(), lr=1e-3)
+model = GPT(vocab_size, d_model, n_heads, d_ff, n_layers, max_len)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
 for step in range(n_epochs):
     # fetches prediction then calculates loss 
     xb, yb = get_batch(data)
-    logits = gpt_model(xb) 
+    logits = model(xb) 
     loss = F.cross_entropy(logits.view(-1, vocab_size), yb.view(-1)) 
 
     # computes gradients and then adjusts weights
@@ -65,6 +66,27 @@ for step in range(n_epochs):
 
     if step % 200 == 0:
         print(f"Epoch: {step}, loss: {loss.item()}")
+
+# saves model checkpoint metrics
+os.makedirs("checkpoints", exist_ok=True)
+
+torch.save({
+    "model": model.state_dict(),
+    "stoi": stoi,
+    "itos": itos,
+    "config": {
+        "vocab size": vocab_size,
+        "d_model": d_model,
+        "n_heads": n_heads,
+        "d_ff": d_ff,
+        "n_layers": n_layers,
+        "max_len": max_len
+    },
+}, "checkpoints/model.pth")
+
+print("Saved checkpoint to 'checkpoints/model.pth'")
+
+
 
 
 
